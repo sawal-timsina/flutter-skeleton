0c1cae59c2bfd9e8dfe60a551e7ee89708ec49e3
Sawal Timsina <t.sawal231@gmail.com>
2022-04-17 21:58:17 +0545

    feat: input field

---
+++ lib/src/widgets/atoms/input_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../config/themes/colors.dart';
+
+class InputField extends StatelessWidget {
+  final Widget child;
+  final String? label;
+  final bool? required;
+  final bool? enabled;
+  final TextStyle? labelStyle;
+  final Color? requiredColor;
+
+  const InputField({
+    Key? key,
+    required this.child,
+    this.label,
+    this.required,
+    this.labelStyle,
+    this.enabled = true,
+    this.requiredColor = AppColors.primary,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          spacing: 8,
+          children: [
+            if (label != null)
+              Text(label!,
+                  style: (labelStyle ?? Theme.of(context).textTheme.subtitle1)!.copyWith(
+                      color: enabled == false ? AppColors.greyDark : null)),
+            if (required == true)
+              Text(
+                "*",
+                style: (labelStyle ?? Theme.of(context).textTheme.subtitle1)?.copyWith(
+                  color: enabled == false ? AppColors.greyDark : requiredColor,
+                ),
+              )
+          ],
+        ),
+        if (label != null) const SizedBox(height: 8),
+        child,
+      ],
+    );
+  }
+}
