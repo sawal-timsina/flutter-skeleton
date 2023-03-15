7577d6bb2e00bc099fc70c5ff61cb15752e28af6
Damodar Bastakoti <damodar.bastakoti9@gmail.com>
2023-03-15 21:00:17 +0545

    feat: added flag color

--- lib/src/config/themes/colors.dart
+++ lib/src/config/themes/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../core/utils/constants.dart';
+
 class AppColors {
   static const MaterialColor primary = MaterialColor(0xFF00826C, <int, Color>{
     50: Color(0xFFE0F0ED),
@@ -19,4 +21,8 @@ class AppColors {
   static const Color secondary = Color(0xFF097693);
   static const Color warning = Color(0xFFBD5D06);
   static const Color error = Color(0xFFA40E15);
+  static List<Color> flagColor = [
+    HexColor.fromHex("#E61F34"),
+    HexColor.fromHex("#002967")
+  ];
 }
