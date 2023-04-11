e601241ae0c0345c1f53df994462fcc026ad7889
Sawal Timsina <52816353+sawal-timsina@users.noreply.github.com>
2023-04-11 17:30:00 +0545

    test

---
+++ lib/bin/git-hooks.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:git_hooks/git_hooks.dart';
+
+void main(List arguments) {
+  var params = {Git.commitMsg: commitMsg, Git.preCommit: preCommit};
+  GitHooks.call(arguments as List<String>, params);
+}
+
+Future<bool> commitMsg() async {
+  return false;
+}
+
+Future<bool> preCommit() async {
+  try {
+    var result = await Process.run('dart', ['analyze']);
+    if (result.exitCode != 0) return false;
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
