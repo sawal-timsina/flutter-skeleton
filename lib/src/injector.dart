bdd4628143666f6afd279d51e10fad857687451d
Aashish-Dahal <ashishdahal490@gmail.com>
2023-12-26 17:37:28 +0545

    feat: commit type checker

--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'config/api/api.dart';
@@ -6,8 +7,11 @@ import 'config/api/api.dart';
 late SharedPreferences sharedPreferences;
 
 Future<void> initializeDependencies() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   EasyLocalization.logger.enableBuildModes = [];
+
+  //GitHooks.init(targetPath: "bin/git_hooks.dart");
 
   // init dio
   InitDio()();
