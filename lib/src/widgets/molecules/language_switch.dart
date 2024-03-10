be879b8884afbae5737600f301daca94fc7415da
Aashish-Dahal <ashishdahal490@gmail.com>
2024-03-10 09:58:03 +0545

    feat: language switch widget

--- lib/src/widgets/molecules/language_switch.dart
+++ lib/src/widgets/molecules/language_switch.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart'
     show BuildContext, Overlay, OverlayEntry, SizedBox, StatelessWidget, Widget;
 
 import '../../../config.dart' show Config;
-import '../../core/utils/constants.dart' show Flavour;
+import '../../core/enums/enums.dart';
 import 'language_settings.dart' show LanguageSettings;
 
 class LanguageSwitch extends StatelessWidget {
-  final Widget? child;
+  final Widget child;
 
-  const LanguageSwitch({super.key, this.child});
+  const LanguageSwitch({super.key, this.child = const SizedBox()});
 
   @override
   Widget build(BuildContext context) {
     return Overlay(
       initialEntries: [
-        OverlayEntry(builder: (_) => child ?? const SizedBox()),
+        OverlayEntry(builder: (_) => child),
         if (Config.flavour == Flavour.development)
           OverlayEntry(builder: (_) => const LanguageSettings()),
       ],
