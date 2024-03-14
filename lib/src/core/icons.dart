4390156b8790022f0282df41c420195dbf0f50ec
Aashish-Dahal <ashishdahal490@gmail.com>
2024-03-14 14:14:38 +0545

    feat: app icons

---
+++ lib/src/core/icons.dart
@@ -0,0 +1,9 @@
+class AppIcons {
+  // static const _iconPath = 'assets/icons';
+  static const _imagePath = 'assets/images';
+
+  // Todo : For Icons
+
+  // Todo: For Images
+  static const noWifi = '$_imagePath/no_wifi.png';
+}
