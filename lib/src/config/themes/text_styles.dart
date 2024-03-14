278d565ff08337c6ab2cd081e66596cea6913966
Aashish-Dahal <ashishdahal490@gmail.com>
2024-03-14 14:13:17 +0545

    feat: app text styles

---
+++ lib/src/config/themes/text_styles.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class AppTextStyles {
+  static const TextStyle displayLarge = TextStyle(
+    color: AppColors.black,
+    fontSize: 25.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle displayMedium = TextStyle(
+    color: AppColors.black,
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle displaySmall = TextStyle(
+    color: AppColors.black,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle headlineLarge = TextStyle(
+    color: AppColors.black,
+    fontSize: 32.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle headlineMedium = TextStyle(
+    color: AppColors.black,
+    fontSize: 28.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle headlineSmall = TextStyle(
+    color: AppColors.black,
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle titleLarge = TextStyle(
+    color: AppColors.black,
+    fontSize: 22.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle titleMedium = TextStyle(
+    color: AppColors.black,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle titleSmall = TextStyle(
+    color: AppColors.black,
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle bodyLarge = TextStyle(
+    color: AppColors.black,
+    fontSize: 18.0,
+    height: 1.0,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    color: AppColors.black,
+    fontSize: 16.0,
+    height: 1.0,
+  );
+
+  static const TextStyle bodySmall = TextStyle(
+    color: AppColors.black,
+    fontSize: 14.0,
+    height: 1.0,
+  );
+
+  static const TextStyle labelLarge = TextStyle(
+    color: AppColors.black,
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle labelMedium = TextStyle(
+    color: AppColors.black,
+    fontSize: 14.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle labelSmall = TextStyle(
+    color: AppColors.black,
+    fontSize: 12.0,
+    fontWeight: FontWeight.bold,
+    height: 1.0,
+  );
+
+  static const TextStyle errorStyle = TextStyle(
+    color: AppColors.error,
+    fontSize: 12.0,
+    height: 1.0,
+  );
+  static const TextStyle hintStyle = TextStyle(
+    color: AppColors.greyDark,
+    fontSize: 12.0,
+    height: 1.0,
+  );
+  static const TextStyle appBarStyle = TextStyle(
+    color: AppColors.white,
+    fontSize: 19.0,
+    height: 1.0,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle dpHeaderHelpStyle = TextStyle(
+    color: AppColors.white,
+    fontSize: 14.0,
+    height: 1.0,
+    fontWeight: FontWeight.normal,
+  );
+}
