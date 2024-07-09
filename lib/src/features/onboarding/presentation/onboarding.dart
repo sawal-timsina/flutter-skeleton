610ddd02aa2995241753507278e39b903ff865f1
Bivek10 <bibekkarki662@gmail.com>
2024-07-09 17:20:41 +0545

    fix: structure onboarding

---
+++ lib/src/features/onboarding/presentation/onboarding.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'onboarding_provider.dart';
+
+class Onboarding extends ConsumerStatefulWidget {
+  static const String routeName = "/onboarding";
+
+  const Onboarding({super.key});
+
+  @override
+  ConsumerState<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends ConsumerState<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Onboarding"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            ref.read(onBoardingProvider.notifier).onboardingFinish();
+          },
+          child: const Text("Finish Onboarding"),
+        ),
+      ),
+    );
+  }
+}
