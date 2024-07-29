027d84ddc7735448d6f9b23738019b55cbb40184
Bivek10 <bibekkarki662@gmail.com>
2024-07-29 18:25:58 +0545

    feat: implement version update checker on app start

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,28 +3,49 @@ import 'package:easy_localization/easy_localization.dart'
 import 'package:flutter/foundation.dart' show Key, Listenable, kDebugMode;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart'
-    show ConsumerWidget, WidgetRef;
+    show ConsumerState, ConsumerStatefulWidget, ConsumerWidget, WidgetRef;
 import 'package:form_builder_validators/form_builder_validators.dart'
     show FormBuilderLocalizations;
 import 'package:sizer/sizer.dart' show Sizer;
 
 import 'config/router/app_router.dart';
 import 'config/themes/app_theme.dart';
+import 'core/version_updator.dart';
 import 'providers/auth_provider.dart';
 import 'providers/onboarding_provider.dart';
 import 'widgets/molecules/language_switch.dart';
 
 final mainNavigator = GlobalKey<NavigatorState>();
 
-class App extends ConsumerWidget {
+class App extends ConsumerStatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> with WidgetsBindingObserver {
   static void dismissKeyboard() {
     FocusManager.instance.primaryFocus?.unfocus();
   }
 
-  const App({Key? key}) : super(key: key);
+  @override
+  void initState() {
+    versionUpdater();
+    super.initState();
+  }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      versionUpdater();
+    }
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
     return Sizer(
       builder: (_, orientation, deviceType) {
         return MaterialApp.router(
